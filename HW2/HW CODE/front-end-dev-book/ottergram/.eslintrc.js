05f2f486ca08546228348470ebf6c9a534ef85c3
sami-halwani1 <sami.halwani1@gmail.com>
2021-09-29 15:51:03 -0700

    OtterGram HTML

---
+++ HW2/HW CODE/front-end-dev-book/ottergram/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+ "env": {
+ "browser": true
+ },
+ "extends": "eslint:recommended", "rules": {
+ "indent": [
+ "error",
+ 2
+],
+"linebreak-style": [
+ "error",
+ "Windows"
+],
+"quotes": [
+ "error",
+ "double"
+],
+"semi": [
+ "error",
+ "always"
+]
+ }
+};
+// NOTE: Change "unix" to "windows" if you are on Windows
---
+++ HW2/HW CODE/front-end-dev-book/ottergram/scripts/main.js
@@ -0,0 +1,124 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
+var BUTTON_NEXT = '[data-image-role = "nextButton"]';
+var BUTTON_PREV = '[data-image-role = "prevButton"]';
+
+var thumbnails = getThumbnailArray();
+var i = 0;
+
+
+function setDetails(imageUrl, titleText) {
+  'use strict';
+
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', imageUrl);
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+
+}
+
+
+function imageFromThumb (thumbnail){
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+
+}
+
+function titleFromThumb (thumbnail){
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+
+}
+
+function addThumbClickHandler(thumb){
+  'use strict';
+  thumb.addEventListener('click', function(event){
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+    i = thumbnails.indexOf(thumb);
+  });
+}
+
+function getThumbnailArray(){
+  'use strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailsArray = [].slice.call(thumbnails);
+  return thumbnailsArray;
+}
+
+function hideDetails(){
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails (){
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function(){
+    frame.classList.remove(TINY_EFFECT_CLASS)
+  }, 50);
+}
+
+function addKeyPressHandler(){
+  'use strict';
+  document.body.addEventListener('keyup', function (event){
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY){
+      hideDetails();
+    }
+  });
+}
+
+function checkNextHandler(cycle){
+  'use strict';
+  cycle.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log(i);
+    if (i <= thumbnails.length -1){
+      setDetailsFromThumb(thumbnails[i+1])
+      showDetails();
+      i++
+    }
+  });
+
+}
+function checkPrevHandler(cycle){
+  'use strict';
+  cycle.addEventListener('click', function(event){
+    event.preventDefault();
+      console.log(i);
+    if (i >= 1){
+      setDetailsFromThumb(thumbnails[i-1])
+      showDetails();
+      i--;
+    }
+  });
+
+}
+
+function initializeEvents() {
+  'use strict';
+  thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
+  checkNextHandler(document.querySelector(BUTTON_NEXT));
+  checkPrevHandler(document.querySelector(BUTTON_PREV));
+
+}
+
+initializeEvents();
